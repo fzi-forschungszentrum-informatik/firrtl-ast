b2273c5a78acd9b4eb00c22d02b54c0ae89dea19
Julian Ganz <ganz@fzi.de>
2021-03-24 12:54:43 +0100

    types: introduce representation of an oriented type

--- src/types.rs
+++ src/types.rs
@@ -51,3 +51,45 @@ pub enum Type {
     Bundle(Vec<(String, Type, Orientation)>),
 }
 
+impl Type {
+    /// Convert to an `OrientedType` with a defined root orientation
+    pub fn with_orientation(&self, orientation: Orientation) -> OrientedType {
+        match self {
+            Self::GroundType(g) => OrientedType::GroundType(*g, orientation),
+            Self::Vector(t, w)  => OrientedType::Vector(Box::new(t.with_orientation(orientation)), *w),
+            Self::Bundle(v)     => OrientedType::Bundle(
+                v.iter().map(|(n, t, o)| (n.clone(), t.with_orientation(*o + orientation))).collect()
+            ),
+        }
+    }
+}
+
+
+/// Oriented type
+///
+/// In an oriented type, the orientation is attached to the leaf nodes, i.e. the
+/// ground types, rather than fields in a bundle.
+#[derive(Clone, Debug)]
+pub enum OrientedType {
+    GroundType(GroundType, Orientation),
+    Vector(Box<OrientedType>, Width),
+    Bundle(Vec<(String, OrientedType)>),
+}
+
+impl OrientedType {
+    /// Clone this type with all orientations flipped
+    pub fn flipped(&self) -> Self {
+        match self {
+            Self::GroundType(g, o) => Self::GroundType(*g, *o + Orientation::Flipped),
+            Self::Vector(t, w)     => Self::Vector(Box::new(t.flipped()), *w),
+            Self::Bundle(v)        => Self::Bundle(v.iter().map(|(n, t)| (n.clone(), t.flipped())).collect()),
+        }
+    }
+}
+
+impl From<&Type> for OrientedType {
+    fn from(t: &Type) -> Self {
+        t.with_orientation(Default::default())
+    }
+}
+
