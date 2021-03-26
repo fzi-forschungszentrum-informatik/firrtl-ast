b0ec477f263d890f8f6dda7778f7965ea7ab4883
Julian Ganz <ganz@fzi.de>
2021-03-26 19:00:50 +0100

    lib: import quickcheck macro

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,10 @@ mod circuit;
 mod module;
 mod types;
 
+#[cfg(test)]
+#[macro_use(quickcheck)]
+extern crate quickcheck_macros;
+
 pub use circuit::Circuit;
 pub use module::{Direction, Module, Port};
 pub use types::{GroundType, Orientation, OrientedType, Type, TypeEq, Width};
