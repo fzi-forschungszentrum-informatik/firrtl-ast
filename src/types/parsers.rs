ebfc5270c53184e646f84dc03430a920217ecb34
Julian Ganz <ganz@fzi.de>
2021-03-29 13:15:25 +0200

    types: add a parser for FIRRTL types

--- src/types/parsers.rs
+++ src/types/parsers.rs
@@ -3,8 +3,9 @@
 use nom::branch::alt;
 use nom::combinator::{map, opt, value};
 use nom::sequence::{preceded, tuple};
+use nom::multi::{fold_many0, separated_list1};
 
-use crate::parsers::{IResult, decimal, kw, op};
+use crate::parsers::{IResult, decimal, identifier, kw, op};
 
 
 /// Parse a ground type
@@ -21,5 +22,23 @@ pub fn ground_type(input: &str) -> IResult<super::GroundType> {
         value(G::Clock, kw("Clock")),
         map(preceded(kw("Analog"), bitwidth), G::Analog),
     ))(input)
+}
+
+
+/// Parse a type
+pub fn r#type(input: &str) -> IResult<super::Type> {
+    use super::Type as T;
+
+    let field = map(
+        tuple((opt(kw("flip")), identifier, op(":"), r#type)),
+        |(o, n, _, t)| (n.to_string(), t, o.map(|_| super::Orientation::Flipped).unwrap_or_default())
+    );
+
+    let (input, res) = alt((
+        map(tuple((op("{"), separated_list1(op(","), field), op("}"))), |(_, v, _)| T::Bundle(v)),
+        map(ground_type, T::GroundType),
+    ))(input)?;
+
+    fold_many0(tuple((op("["), decimal, op("]"))), res, |t, (_, w, _)| T::Vector(Box::new(t), w))(input)
 }
 
