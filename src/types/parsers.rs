60351dde38bbb0917d6bd9329d496e5a45a57b50
Julian Ganz <ganz@fzi.de>
2021-04-12 15:55:02 +0200

    types: migrate type parsers to explicit handling of spaces
    Previously, we would have our base parsers simply consume all preceding
    spaces. However, this turned out to be an ill choice as FIRRTL is one of
    those languages where the indentation has meaning. We therefore decided
    to move all that whitespace consumption out of the base parsers.
    
    This change migrates the parsers for both ground types and complex
    types. As a result, the parsers no longer consume preceding whitespace.

--- src/types/parsers.rs
+++ src/types/parsers.rs
@@ -5,15 +5,15 @@ use nom::combinator::{map, opt, value};
 use nom::sequence::{preceded, tuple};
 use nom::multi::{fold_many0, separated_list1};
 
-use crate::parsers::{IResult, decimal, identifier, kw, op};
+use crate::parsers::{IResult, decimal, identifier, kw, op, spaced};
 
 
 /// Parse a ground type
 pub fn ground_type(input: &str) -> IResult<super::GroundType> {
     use super::GroundType as G;
 
-    let bitwidth = |i| opt(map(tuple((op("<"), decimal, op(">"))), |(_, w, _)| w))(i);
-    let point_offset = |i| opt(map(tuple((op("<<"), decimal, op(">>"))), |(_, w, _)| w))(i);
+    let bitwidth = |i| opt(map(spaced(tuple((op("<"), decimal, op(">")))), |(_, w, _)| w))(i);
+    let point_offset = |i| opt(spaced(map(tuple((op("<<"), decimal, op(">>"))), |(_, w, _)| w)))(i);
 
     alt((
         map(preceded(kw("UInt"), bitwidth), G::UInt),
@@ -30,15 +30,22 @@ pub fn r#type(input: &str) -> IResult<super::Type> {
     use super::Type as T;
 
     let field = map(
-        tuple((opt(kw("flip")), identifier, op(":"), r#type)),
+        tuple((opt(kw("flip")), spaced(identifier), spaced(op(":")), spaced(r#type))),
         |(o, n, _, t)| (n.to_string(), t, o.map(|_| super::Orientation::Flipped).unwrap_or_default())
     );
 
     let (input, res) = alt((
-        map(tuple((op("{"), separated_list1(op(","), field), op("}"))), |(_, v, _)| T::Bundle(v)),
+        map(
+            tuple((op("{"), separated_list1(spaced(op(",")), spaced(field)), spaced(op("}")))),
+            |(_, v, _)| T::Bundle(v)
+        ),
         map(ground_type, T::GroundType),
     ))(input)?;
 
-    fold_many0(tuple((op("["), decimal, op("]"))), res, |t, (_, w, _)| T::Vector(Box::new(t), w))(input)
+    fold_many0(
+        spaced(tuple((op("["), spaced(decimal), spaced(op("]"))))),
+        res,
+        |t, (_, w, _)| T::Vector(Box::new(t), w)
+    )(input)
 }
 
