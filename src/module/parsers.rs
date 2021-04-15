345a65997fe0e0504cc3ef12e212a3b1fd50d16c
Julian Ganz <ganz@fzi.de>
2021-04-15 18:18:36 +0200

    module: add parser for Ports

--- src/module/parsers.rs
+++ src/module/parsers.rs
@@ -1,9 +1,21 @@
 //! Parsers for modules and related items
 
 use nom::branch::alt;
-use nom::combinator::value;
+use nom::combinator::{map, value};
+use nom::sequence::tuple;
 
-use crate::parsers::{IResult, kw};
+use crate::parsers::{IResult, identifier, kw, op, spaced};
+use crate::types::Type;
+use crate::types::parsers::r#type;
+
+
+/// Parse the elements of a port
+fn port<'i>(input: &str) -> IResult<(String, Type, super::Direction)> {
+    map(
+        tuple((direction, spaced(identifier), spaced(op(":")), spaced(r#type))),
+        |(direction, name, _, r#type)| (name.to_string(), r#type, direction)
+    )(input)
+}
 
 
 /// Parse a direction
