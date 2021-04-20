16f7305f8ccf250b457445bbfc53b3c0014f45eb
Julian Ganz <ganz@fzi.de>
2021-04-20 18:15:31 +0200

    module: add test for Module parser

--- src/module/tests.rs
+++ src/module/tests.rs
@@ -3,9 +3,23 @@
 use nom::Finish;
 use nom::combinator::all_consuming;
 
+use crate::indentation::{DisplayIndented, Indentation};
 use crate::tests::Equivalence;
 
-use super::{Direction, parsers};
+use super::{Direction, Module, parsers};
+
+
+#[quickcheck]
+fn parse_module(mut base: Indentation, original: Module) -> Result<Equivalence<Module>, String> {
+    let mut s: String = Default::default();
+    original.fmt(&mut base, &mut s).map_err(|e| e.to_string())?;
+
+    let res = all_consuming(|i| parsers::module(i, &mut base))(&s)
+        .finish()
+        .map(|(_, parsed)| Equivalence::of(original, parsed))
+        .map_err(|e| e.to_string());
+    res
+}
 
 
 #[quickcheck]
