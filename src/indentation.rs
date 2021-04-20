974428aac3c8280450df5891d05029805e72696c
Julian Ganz <ganz@fzi.de>
2021-04-20 14:42:32 +0200

    indentation: create a type representing indentation
    FIRRTL is one of those languages where indentation is syntactically
    relevant. Therefore, we need to make sure we follow the indentation
    rules when printing FIRRTL and we also need to recognize indentation
    when parsing.
    
    This change introduces a new module. In the future, it will provide
    utilities for both printing and parsing indentation. This change also
    introduces a type for representing an indentation level.

---
+++ src/indentation.rs
@@ -0,0 +1,76 @@
+//! Indentation utilities
+
+use std::num::NonZeroUsize;
+
+
+/// Indentation
+///
+/// Instances of this type represent either a lower bound or an exact length of
+/// a sequences of space characters. Usually, a new `Indentation` will represent
+/// a lower bound. After an instance has been locked, it will always refer to
+/// the same exact length.
+#[derive(Clone, Debug, PartialEq)]
+pub enum Indentation {
+    MoreThan(usize),
+    Exact(usize),
+}
+
+impl Indentation {
+    /// Lock the indentation to a concrete value
+    ///
+    /// If the `Indentation` represents only a lower bound, this function will
+    /// set an exact value which will be `steps` above the parent indentation
+    /// level. If the `Indentation` is already locked, this function doesn't
+    /// have any effect.
+    ///
+    /// The function returns a `LockedIndentation` reflecting the excact
+    /// indentation length.
+    pub fn lock_with(&mut self, steps: NonZeroUsize) -> LockedIndentation {
+        match self {
+            Self::MoreThan(i) => {
+                let i = *i + steps.get();
+                *self = Self::Exact(i);
+                LockedIndentation(i)
+            },
+            Self::Exact(i) => LockedIndentation(*i),
+        }
+    }
+
+    /// Create a new, locked "root"
+    ///
+    /// The `Indentation` returned will be locked to a length of `0`, i.e. no
+    /// indentation at all.
+    pub fn root() -> Self {
+        Self::Exact(0)
+    }
+}
+
+impl Default for Indentation {
+    fn default() -> Self {
+        Self::root()
+    }
+}
+
+
+#[derive(Copy, Clone, Debug)]
+pub struct LockedIndentation(usize);
+
+impl LockedIndentation {
+    /// Create a new indentation with a lower (i.e. nested) level
+    pub fn sub(&self) -> Indentation {
+        Indentation::MoreThan(self.into())
+    }
+}
+
+impl From<&LockedIndentation> for usize {
+    fn from(i: &LockedIndentation) -> Self {
+        i.0
+    }
+}
+
+impl From<LockedIndentation> for usize {
+    fn from(i: LockedIndentation) -> Self {
+        i.0
+    }
+}
+
