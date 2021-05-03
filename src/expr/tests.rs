c41e72f2a4b43d28ee6cfc0c144fe51a6bfdbfdc
Julian Ganz <ganz@fzi.de>
2021-05-03 14:57:27 +0200

    expr: add test for primitive operation parser
    The primitive operation parser should already be covered well through
    the expression parser test. However, failures will usually be somewhat
    obscure since the parser will resort to parsing identifiers and hence,
    at least at the moment, produces suboptimal failure reports since all
    primitive operations start with a string which _could_ also be a valid
    identifier.

--- src/expr/tests.rs
+++ src/expr/tests.rs
@@ -5,13 +5,26 @@ use nom::Finish;
 
 use crate::tests::{Equivalence, Identifier};
 
-use super::{Expression, parsers};
+use super::{Expression, parsers, primitive};
 
 
 #[quickcheck]
 fn parse_expr(original: Expression<Identifier>) -> Result<Equivalence<Expression<Identifier>>, String> {
     let s = original.to_string();
     let res = all_consuming(|i| parsers::expr(|s| Some(s.into()), i))(&s)
+        .finish()
+        .map(|(_, parsed)| Equivalence::of(original, parsed))
+        .map_err(|e| e.to_string());
+    res
+}
+
+
+#[quickcheck]
+fn parse_primitive_op(
+    original: primitive::Operation<Identifier>
+) -> Result<Equivalence<primitive::Operation<Identifier>>, String> {
+    let s = original.to_string();
+    let res = all_consuming(|i| parsers::primitive_op(|s| Some(s.into()), i))(&s)
         .finish()
         .map(|(_, parsed)| Equivalence::of(original, parsed))
         .map_err(|e| e.to_string());
