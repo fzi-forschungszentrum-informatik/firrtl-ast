c388dd5243e9bc3da45cbdcfd7a83bdf1d6246ac
Julian Ganz <ganz@fzi.de>
2021-05-06 16:02:55 +0200

    types: move OrientedType into its own submodule
    The `types` top-level module has grown rather big and is likely to gain
    new members in the future. Hence, we decided to move some parts of it
    into sub-modules.
    
    This change moves `OrientedType` into a new sub-module.

--- src/types.rs
+++ src/types.rs
@@ -3,6 +3,7 @@
 pub mod parsers;
 
 mod display;
+mod oriented;
 
 #[cfg(test)]
 mod tests;
@@ -13,6 +14,8 @@ use std::sync::Arc;
 
 #[cfg(test)]
 use quickcheck::{Arbitrary, Gen};
+
+pub use oriented::OrientedType;
 
 
 /// Bit-width of a ground-type, i.e. the number of "physical" wires or signals
@@ -313,58 +316,6 @@ impl Arbitrary for BundleField {
 }
 
 
-
-
-/// Oriented type
-///
-/// In an oriented type, the orientation is attached to the leaf nodes, i.e. the
-/// ground types, rather than fields in a bundle.
-#[derive(Clone, PartialEq, Debug)]
-pub enum OrientedType {
-    GroundType(GroundType, Orientation),
-    Vector(Arc<Self>, VecWidth),
-    Bundle(Arc<[(Arc<str>, Self)]>),
-}
-
-impl OrientedType {
-    /// Clone this type with all orientations flipped
-    pub fn flipped(&self) -> Self {
-        match self {
-            Self::GroundType(g, o) => Self::GroundType(*g, *o + Orientation::Flipped),
-            Self::Vector(t, w)     => Self::Vector(Arc::new(t.flipped()), *w),
-            Self::Bundle(v)        => Self::Bundle(v.iter().map(|(n, t)| (n.clone(), t.flipped())).collect()),
-        }
-    }
-}
-
-impl TypeExt for OrientedType {
-    fn eq(&self, rhs: &Self) -> bool {
-        match (self, rhs) {
-            (Self::GroundType(t1, o1), Self::GroundType(t2, o2)) => TypeExt::eq(t1, t2) && o1 == o2,
-            (Self::Vector(t1, w1), Self::Vector(t2, w2)) => TypeExt::eq(t1.as_ref(), t2.as_ref()) && w1 == w2,
-            (Self::Bundle(v1), Self::Bundle(v2)) => if v1.len() == v2.len() {
-                v1.iter().zip(v2.iter()).all(|((n1, t1), (n2, t2))| n1 == n2 && TypeExt::eq(t1, t2))
-            } else {
-                false
-            },
-            _ => false
-        }
-    }
-
-    fn is_passive(&self) -> bool {
-        match self {
-            Self::GroundType(t, o) => t.is_passive() && *o == Orientation::Normal,
-            Self::Vector(t, _) => t.is_passive(),
-            Self::Bundle(v) => v.iter().all(|(_, t)| t.is_passive()),
-        }
-    }
-}
-
-impl From<&Type> for OrientedType {
-    fn from(t: &Type) -> Self {
-        t.with_orientation(Default::default())
-    }
-}
 
 
 /// Trait representing common FIRRTL type concepts
