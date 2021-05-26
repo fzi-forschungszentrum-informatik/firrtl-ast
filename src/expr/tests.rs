b6caf85b1c6975ab1f3cd20cbf21614a1ebac59c
Julian Ganz <ganz@fzi.de>
2021-05-26 14:32:11 +0200

    expr: add functions for constructing some type variants
    During testing, we generate arbitrary expressions. Currently, we do
    not respect typing constraints. Thus, we may generate expressions which
    are invalid, e.g. a sub-index on a UInt-literal. We plan on changing
    this, i.e. generating expressions which could at least be typed
    correctly.
    
    During generation, we naturally need to generate sub-expressions. Their
    types are related to the overall expressions, i.e. we need to derive the
    sub-expression's type from the type of the current node. The functions
    introduced with this chage function as helpers for this purpose.

--- src/expr/tests.rs
+++ src/expr/tests.rs
@@ -1,8 +1,10 @@
 //! Test related to expressions
 
+use std::sync::Arc;
+
 use nom::combinator::all_consuming;
 use nom::Finish;
-use quickcheck::Gen;
+use quickcheck::{Arbitrary, Gen};
 
 use crate::tests::{Equivalence, Identifier};
 use crate::types;
@@ -38,5 +40,20 @@ fn parse_primitive_op(
 pub trait TypedRef: super::Reference {
     /// Generate a reference with the given type
     fn with_type(r#type: types::Type, g: &mut Gen) -> Self;
+}
+
+
+/// Generate a bundle type with a field constructed from the given type and name
+fn bundle_with_field(r#type: types::Type, name: Arc<str>, g: &mut Gen) -> types::Type {
+    let mut fields = types::bundle_fields(u8::arbitrary(g) as usize, g);
+    let field = types::BundleField::new(name.clone(), r#type, Arbitrary::arbitrary(g));
+    fields.insert(name, field);
+    fields.into()
+}
+
+
+/// Generate a vector type with the given base/item type
+fn vec_with_base(r#type: types::Type, g: &mut Gen) -> types::Type {
+    types::Type::Vector(Arc::new(r#type), Arbitrary::arbitrary(g))
 }
 
