6b4b10d5129205279594e7c551356a1a06bc03fe
Julian Ganz <ganz@fzi.de>
2021-06-11 18:18:44 +0200

    register: impl quickcheck::Arbitrary for Register

--- src/register.rs
+++ src/register.rs
@@ -5,6 +5,9 @@ pub(crate) mod parsers;
 use std::fmt;
 use std::sync::Arc;
 
+#[cfg(test)]
+use quickcheck::{Arbitrary, Gen};
+
 use crate::expr;
 use crate::types;
 
@@ -92,6 +95,86 @@ impl<R: expr::Reference> fmt::Display for Register<R> {
             write!(f, " with: (reset => ({}, {}))", sig, val)?;
         }
         Ok(())
+    }
+}
+
+#[cfg(test)]
+impl<R: expr::tests::TypedRef + Clone + 'static> Arbitrary for Register<R> {
+    fn arbitrary(g: &mut Gen) -> Self {
+        use crate::tests::Identifier;
+        use expr::tests::expr_with_type;
+
+        let res = Self::new(
+            Identifier::arbitrary(g),
+            types::Type::arbitrary(g),
+            expr_with_type(types::GroundType::Clock, g),
+        );
+
+        if bool::arbitrary(g) {
+            let val_type = res.r#type.clone();
+            res.with_reset(expr_with_type(types::GroundType::UInt(Some(1)), g), expr_with_type(val_type, g))
+        } else {
+            res
+        }
+    }
+
+    fn shrink(&self) -> Box<dyn Iterator<Item = Self>> {
+        use types::{GroundType as GT};
+        use expr::tests::TypedExpr;
+
+        let res = crate::tests::Identifier::from(self.name.as_ref())
+            .shrink()
+            .map({
+                let t = self.r#type.clone();
+                let c = self.clock.clone();
+                let r = self.reset.clone();
+                move |n| Self::new(n, t.clone(), c.clone()).with_optional_reset(r.clone())
+            })
+            .chain(self.r#type.shrink().map({
+                // When shrinking the type, it's unlikely that we'll find a
+                // suitable sub-expression for the reset-value and generating
+                // one is just plain wrong. We take the easy way out and just
+                // leave out the reset all together.
+                let n = self.name.clone();
+                let c = self.clock.clone();
+                move |t| Self::new(n.clone(), t, c.clone())
+            }))
+            .chain(TypedExpr {expr: self.clock.clone(), r#type: GT::Clock.into()}
+                .shrink()
+                .filter(|c| c.r#type == GT::Clock.into())
+                .map({
+                    let n = self.name.clone();
+                    let t = self.r#type.clone();
+                    let r = self.reset.clone();
+                    move |c| Self::new(n.clone(), t.clone(), c.expr).with_optional_reset(r.clone())
+                })
+            );
+
+        if let Some((sig, val)) = self.reset.as_ref() {
+            let n = self.name.clone();
+            let c = self.clock.clone();
+
+            let r_shrink = TypedExpr {expr: sig.clone(), r#type: GT::UInt(Some(1)).into()}
+                .shrink()
+                .filter(|sig| sig.r#type == GT::UInt(Some(1)).into())
+                .map({
+                    let val = val.clone();
+                    let t = self.r#type.clone();
+                    move |sig| (sig.expr, val.clone(), t.clone())
+                })
+                .chain(TypedExpr {expr: val.clone(), r#type: self.r#type.clone()}
+                    .shrink()
+                    .map({
+                        let sig = sig.clone();
+                        move |val| (sig.clone(), val.expr, val.r#type)
+                    })
+                )
+                .map(move |(s, v, t)| Self::new(n.clone(), t, c.clone()).with_optional_reset(Some((s, v))));
+
+            Box::new(res.chain(r_shrink).chain(std::iter::once(self.clone().without_reset())))
+        } else {
+            Box::new(res)
+        }
     }
 }
 
