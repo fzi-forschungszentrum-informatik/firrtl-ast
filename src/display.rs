7c38e71bf2c682dcdef194818da9d3e16ef2f93f
Julian Ganz <ganz@fzi.de>
2021-06-16 15:00:55 +0200

    display: widen contract for CommaSeparator to allow IntoIterator + Clone
    The recently introduced `CommaSeparator` turned out to be not as useful
    as we thought it would be, as it did not allow filtering and/or mapping
    items prior to printing. Collecting them before instantiation was
    possible, but would introduce unnecessary overhead.
    
    Hence, we decided to make it accept types which implement `IntoIterator`
    with a displayable item type. Since `Display` mandates that we do not
    mutate the struct, we further require that type to implement `Clone`.

--- src/display.rs
+++ src/display.rs
@@ -1,23 +1,38 @@
 //! Genderal display utilities
 
 use std::fmt;
+use std::sync::Arc;
 
 
 /// Utility for formatting comma separated lists
-pub struct CommaSeparated<'a, T: fmt::Display>(&'a [T]);
-
-impl<'a, R, T> From<&'a R> for CommaSeparated<'a, T>
-where R: AsRef<[T]>,
-      T: fmt::Display,
+pub struct CommaSeparated<I, E>
+where I: IntoIterator<Item = E> + Clone,
+      E: fmt::Display,
 {
-    fn from(vec: &'a R) -> Self {
-        Self(vec.as_ref())
+    inner: I,
+}
+
+impl<'a, E: fmt::Display> From<&'a Arc<[E]>> for CommaSeparated<&'a [E], &'a E> {
+    fn from(inner: &'a Arc<[E]>) -> Self {
+        Self{inner: inner.as_ref()}
     }
 }
 
-impl<'a, T: fmt::Display> fmt::Display for CommaSeparated<'a, T> {
+impl<I, E> From<I> for CommaSeparated<I, E>
+where I: IntoIterator<Item = E> + Clone,
+      E: fmt::Display,
+{
+    fn from(inner: I) -> Self {
+        Self{inner}
+    }
+}
+
+impl<I, E> fmt::Display for CommaSeparated<I, E>
+where I: IntoIterator<Item = E> + Clone,
+      E: fmt::Display,
+{
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let mut items = self.0.iter();
+        let mut items = self.inner.clone().into_iter();
         items.next().map(|item| item.fmt(f)).transpose().map(|_| ())?;
         items.try_for_each(|item| write!(f, ", {}", item))
     }
