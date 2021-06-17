20695038074fa56803640e503b4ae5c9c399670e
Julian Ganz <ganz@fzi.de>
2021-06-17 16:00:28 +0200

    register: use now public utility parsers for parsing registers

--- src/register/parsers.rs
+++ src/register/parsers.rs
@@ -5,7 +5,7 @@ use nom::sequence::tuple;
 
 use crate::expr::Reference;
 use crate::expr::parsers::expr;
-use crate::parsers::{IResult, identifier, kw, op, spaced};
+use crate::parsers::{IResult, comma, identifier, kw, lp, op, rp, spaced};
 use crate::types::parsers::r#type;
 
 
@@ -14,33 +14,28 @@ pub fn register<'i, R: Reference + Clone>(
     reference: impl Fn(&str) -> Option<R> + Copy,
     input: &'i str
 ) -> IResult<'i, super::Register<R>> {
+    use nom::Parser;
+
+    let expr = |i| spaced(|i| expr(reference, i)).parse(i);
+
     let reset = map(
-        tuple((
-            spaced(op("(")),
-            spaced(kw("reset")),
-            spaced(op("=>")),
-            spaced(op("(")),
-            spaced(|i| expr(reference, i)),
-            spaced(op(",")),
-            spaced(|i| expr(reference, i)),
-            spaced(op(")")),
-            spaced(op(")")),
-        )),
+        tuple((lp, spaced(kw("reset")), spaced(op("=>")), lp, &expr, comma, &expr, rp, rp)),
         |(.., sig, _, val, _, _)| (sig, val)
     );
 
-    map(
+    let res = map(
         tuple((
             kw("reg"),
             spaced(identifier),
             spaced(op(":")),
             spaced(r#type),
-            spaced(op(",")),
-            spaced(|i| expr(reference, i)),
+            comma,
+            &expr,
             opt(spaced(map(tuple((kw("with"), spaced(op(":")), spaced(reset))), |(.., r)| r)))
         )),
         |(_, name, _, r#type, _, clock, reset)| super::Register::new(name, r#type, clock)
             .with_optional_reset(reset)
-    )(input)
+    )(input);
+    res
 }
 
