82ebe26cf6ff475a5331da7430dac1535235ccac
Julian Ganz <ganz@fzi.de>
2021-06-23 16:54:56 +0200

    stmt: add test for Statement parser

--- src/stmt/tests.rs
+++ src/stmt/tests.rs
@@ -11,7 +11,39 @@ use crate::expr::{Expression, Reference};
 use crate::indentation::{DisplayIndented, Indentation};
 use crate::tests::Equivalence;
 
-use super::Entity;
+use super::{Entity, Statement};
+
+
+#[quickcheck]
+fn parse_stmt(mut base: Indentation, original: Statement) -> Result<TestResult, String> {
+    // We depend on reference and module names to be unique. If they are not,
+    // the set of names will be smaller than the corresponding list.
+    let refs: Vec<_> = stmt_exprs(&original).into_iter().flat_map(Expression::references).collect();
+    if refs.iter().map(|r| r.name()).collect::<std::collections::HashSet<_>>().len() != refs.len() {
+        return Ok(TestResult::discard())
+    }
+
+    let mods = stmt_modules(&original);
+    if mods.iter().map(|r| r.name()).collect::<std::collections::HashSet<_>>().len() != refs.len() {
+        return Ok(TestResult::discard())
+    }
+
+    let mut s: String = Default::default();
+    original.fmt(&mut base, &mut s).map_err(|e| e.to_string())?;
+
+    let parser = move |i| super::parsers::stmt(
+        |n| refs.iter().find(|r| r.name() == n).cloned(),
+        |n| mods.iter().find(|m| m.name() == n).cloned(),
+        i,
+        &mut base
+    );
+
+    let res = all_consuming(parser)(&s)
+        .finish()
+        .map(|(_, parsed)| Equivalence::of(original, parsed).result(&mut Gen::new(0)))
+        .map_err(|e| e.to_string());
+    res
+}
 
 
 #[quickcheck]
@@ -48,6 +80,50 @@ fn parse_entity(mut base: Indentation, original: Entity) -> Result<TestResult, S
         .map(|(_, parsed)| Equivalence::of(original, parsed).result(&mut Gen::new(0)))
         .map_err(|e| e.to_string());
     res
+}
+
+
+/// Retrieve all expressions occuring in a statement
+fn stmt_exprs(stmt: &Statement) -> Vec<&Expression<Arc<Entity>>> {
+    match stmt {
+        Statement::Connection{from, to}             => vec![from, to],
+        Statement::PartialConnection{from, to}      => vec![from, to],
+        Statement::Empty                            => Default::default(),
+        Statement::Declaration(entity)              => entity_exprs(entity.as_ref()),
+        Statement::Invalidate(expr)                 => vec![expr],
+        Statement::Attach(v)                        => v.iter().collect(),
+        Statement::Conditional{cond, when, r#else}  => std::iter::once(cond)
+            .chain(when.iter().flat_map(stmt_exprs))
+            .chain(r#else.iter().flat_map(stmt_exprs))
+            .collect(),
+        Statement::Stop{clock, cond, ..}            => vec![clock, cond],
+        Statement::Print{clock, cond, msg}          => std::iter::once(clock)
+            .chain(std::iter::once(cond))
+            .chain(msg.iter().filter_map(|p| if let super::PrintElement::Value(e, _) = p {
+                Some(e)
+            } else {
+                None
+            }))
+            .collect(),
+    }
+}
+
+
+/// Retrieve all modules instanciated in a statement
+fn stmt_modules(stmt: &Statement) -> Vec<Arc<crate::module::Module>> {
+    match stmt {
+        Statement::Declaration(m) => if let Entity::Instance(m) = m.as_ref() {
+            vec![m.module().clone()]
+        } else {
+            Default::default()
+        },
+        Statement::Conditional{when, r#else, ..} => when
+            .iter()
+            .flat_map(stmt_modules)
+            .chain(r#else.iter().flat_map(stmt_modules))
+            .collect(),
+        _ => Default::default(),
+    }
 }
 
 
