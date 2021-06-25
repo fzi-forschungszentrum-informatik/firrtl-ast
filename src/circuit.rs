78dddf8e3dbab1efc50fa9dfb32235af12065f0e
Julian Ganz <ganz@fzi.de>
2021-06-25 17:52:29 +0200

    circuit: add iterator wrapper for grabbing the top module

--- src/circuit.rs
+++ src/circuit.rs
@@ -53,3 +53,63 @@ impl Arbitrary for Circuit {
     }
 }
 
+
+/// Iterator adapter/wrapper for creating a circuit
+///
+/// Instances of this type wrap an iterator over `Module`s. It allows iterating
+/// over the modules yielded by the inner iterator transparently, seeking out
+/// the top module for a target circuit by name.
+#[derive(Clone, Debug)]
+pub struct ModuleConsumer<I: Iterator<Item = Arc<Module>>> {
+    top_module: TopState,
+    modules: I,
+}
+
+impl<I: Iterator<Item = Arc<Module>>> ModuleConsumer<I> {
+    /// Create a new adapter for the given target top module name
+    pub fn new(top_name: impl Into<String>, modules: I) -> Self {
+        Self {top_module: TopState::Name(top_name.into()), modules}
+    }
+
+    /// Retrieve the circuit
+    ///
+    /// If the top module was collected, this function returns the circuit,
+    /// otherwise `None` will be returned.
+    pub fn circuit(&self) -> Option<Circuit> {
+        if let TopState::Module(m) = &self.top_module {
+            Some(Circuit::new(m.clone()))
+        } else {
+            None
+        }
+    }
+
+    /// Try to create the requested circuit, consuming the iterator
+    pub fn into_circuit(mut self) -> Option<Circuit> {
+        match self.top_module {
+            TopState::Name(n)   => self.modules.find(|m| m.name() == n),
+            TopState::Module(m) => Some(m),
+        }.map(Circuit::new)
+    }
+}
+
+impl<I: Iterator<Item = Arc<Module>>> Iterator for ModuleConsumer<I> {
+    type Item = I::Item;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let res = self.modules.next();
+        if let (Some(m), TopState::Name(n)) = (res.as_ref(), &self.top_module) {
+            if n == m.name() {
+                self.top_module = TopState::Module(m.clone())
+            }
+        }
+        res
+    }
+}
+
+
+#[derive(Clone, Debug)]
+enum TopState {
+    Name(String),
+    Module(Arc<Module>),
+}
+
