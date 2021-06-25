d9caa577f2d81536d00306924bb8b1877618c20c
Julian Ganz <ganz@fzi.de>
2021-06-25 18:17:44 +0200

    circuit: rewrite circuit parser based on the ModuleConsumer
    The parser will now yield the circuit right after the top module was
    parsed. I.e. it will not parse all modules and search for the top-module
    afterwards.

--- src/circuit/parsers.rs
+++ src/circuit/parsers.rs
@@ -2,30 +2,27 @@
 
 use std::sync::Arc;
 
-use nom::combinator::{map, map_opt};
-use nom::multi::many1;
+use nom::combinator::{iterator, map};
 use nom::sequence::tuple;
 
 use crate::indentation::Indentation;
 use crate::module::parsers::module;
-use crate::parsers::{IResult, identifier, kw, le, op, spaced};
+use crate::parsers::{Error, IResult, identifier, kw, le, op, spaced};
 
 
 /// Parse a Circuit
 pub fn circuit(input: &str) -> IResult<super::Circuit> {
+    use nom::error::{ErrorKind as EK, ParseError};
+
+    let (input, top_name) = map(
+        tuple((kw("circuit"), spaced(identifier), spaced(op(":")), le)),
+        |(_, n, ..)| n
+    )(input)?;
+
     let mut indent = Indentation::root().sub();
-    let res = map_opt(
-        tuple((
-            kw("circuit"),
-            spaced(identifier),
-            spaced(op(":")),
-            le,
-            many1(map(|i| module(i, &mut indent), Arc::new))
-        )),
-        |(_, top_name, .., modules)| {
-            let top = modules.iter().find(|m| m.name() == top_name)?.clone();
-            Some(super::Circuit::new(top))
-        }
-    )(input);
-    res
+    let mut modules = iterator(input, map(|i| module(i, &mut indent), Arc::new));
+    let res = super::ModuleConsumer::new(top_name, &mut modules)
+        .into_circuit()
+        .ok_or_else(|| nom::Err::Error(Error::from_error_kind(input, EK::MapOpt)))?;
+    modules.finish().map(|(i, _)| (i, res))
 }
