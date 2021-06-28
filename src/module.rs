b8cacfa76f3710010a1dfd27d177786cfc9f897d
Julian Ganz <ganz@fzi.de>
2021-06-28 14:55:52 +0200

    module: make DisplayIndented impl for Module reflect Kind and statements

--- src/module.rs
+++ src/module.rs
@@ -81,9 +81,10 @@ impl Module {
 
 impl DisplayIndented for Module {
     fn fmt<W: fmt::Write>(&self, indentation: &mut Indentation, f: &mut W) -> fmt::Result {
-        writeln!(f, "{}module {}:", indentation.lock(), self.name())?;
+        writeln!(f, "{}{} {}:", indentation.lock(), self.kind(), self.name())?;
         let mut indentation = indentation.sub();
-        self.ports().try_for_each(|p| DisplayIndented::fmt(p, &mut indentation, f))
+        self.ports().try_for_each(|p| DisplayIndented::fmt(p, &mut indentation, f))?;
+        self.statements().iter().try_for_each(|s| DisplayIndented::fmt(s, &mut indentation, f))
     }
 }
 
