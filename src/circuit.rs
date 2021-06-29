ee01e06ef66fe208631165372f1ec41e8eb28774
Julian Ganz <ganz@fzi.de>
2021-06-29 14:43:02 +0200

    circuit: recursively print all dependencies of the top module
    In the past, a circuit would be completely defined by its top module.
    However, we recently allowed `Module`s to carry statements, including
    instantiations of modules.
    
    As a circuit is meant to be self-sufficient, we need to include all the
    dependencies of a formatted module, i.e. all modules instantiated in it.

--- src/circuit.rs
+++ src/circuit.rs
@@ -38,11 +38,29 @@ impl Circuit {
 
 impl fmt::Display for Circuit {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        use indentation::DisplayIndented;
+        use std::collections::HashSet;
+        use indentation::{DisplayIndented, Indentation};
+
+        // Format a module and all its dependencies, if it wasn't yet formatted
+        fn fmt_module<'a>(
+            done: &mut HashSet<&'a str>,
+            indent: &mut Indentation,
+            module: &'a Module,
+            f: &mut fmt::Formatter<'_>,
+        ) -> fmt::Result {
+            if done.insert(module.name()) {
+                module.referenced_modules().try_for_each(|m| fmt_module(done, indent, m, f))?;
+                module.fmt(indent, f)
+            } else {
+                Ok(())
+            }
+        }
+
+        let mut done = Default::default();
 
         writeln!(f, "circuit {}:", self.top_module().name())?;
         let mut indent = indentation::Indentation::root().sub();
-        self.top_module().fmt(&mut indent, f)
+        fmt_module(&mut done, &mut indent, self.top_module(), f)
     }
 }
 
