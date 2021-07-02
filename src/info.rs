73eb21cb83ca0b719d26509f97eb78f7993bd3a4
Julian Ganz <ganz@fzi.de>
2021-07-02 14:54:49 +0200

    info: add formatting utility for info attributes

--- src/info.rs
+++ src/info.rs
@@ -1,5 +1,7 @@
 //! Info attribute
 
+use std::fmt;
+
 
 /// Trait providing access to attached info
 ///
@@ -26,6 +28,48 @@ pub trait WithInfo {
     /// Clear the attached info
     fn clear_info(&mut self) {
         self.set_info(None)
+    }
+}
+
+
+/// Helper for formatting an entities info attribute
+#[derive(Clone, Default)]
+pub(crate) struct Info<'a>(pub Option<&'a str>);
+
+impl<'a> Info<'a> {
+    /// Create a formatting helper for the info of the given entity
+    pub fn of(entity: &'a impl WithInfo) -> Self {
+        entity.info().into()
+    }
+}
+
+impl<'a> From<&'a str> for Info<'a> {
+    fn from(i: &'a str) -> Self {
+        Some(i).into()
+    }
+}
+
+impl<'a> From<Option<&'a str>> for Info<'a> {
+    fn from(i: Option<&'a str>) -> Self {
+        Self(i)
+    }
+}
+
+impl fmt::Display for Info<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        if let Some(s) = self.0 {
+            write!(f, " @[")?;
+            s.chars().try_for_each(|c| match c {
+                '\n' => write!(f, "\\\n"),
+                '\t' => write!(f, "\\\t"),
+                ']'  => write!(f, "\\]"),
+                '\\' => write!(f, "\\\\"),
+                c    => fmt::Display::fmt(&c, f),
+            })?;
+            write!(f, "]")
+        } else {
+            Ok(())
+        }
     }
 }
 
