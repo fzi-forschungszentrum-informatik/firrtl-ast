87fbbb2307d5cfd26d96c7d6e33c3759a9184029
Julian Ganz <ganz@fzi.de>
2021-07-05 14:57:38 +0200

    stmt: use MemoryDecl for formatting Memory declarations
    For now, we will not supply an info attribute. A future change will
    change this.

--- src/stmt/display.rs
+++ src/stmt/display.rs
@@ -10,13 +10,16 @@ pub struct EntityDecl<'a>(pub &'a super::Entity);
 
 impl DisplayIndented for EntityDecl<'_> {
     fn fmt<W: fmt::Write>(&self, indentation: &mut Indentation, f: &mut W) -> fmt::Result {
+        use crate::memory::display::MemoryDecl;
+
         use super::Entity as E;
+
         match self.0 {
             E::Port(_)              => Err(Default::default()),
             E::Wire{name, r#type}   => writeln!(f, "{}wire {}: {}", indentation.lock(), name, r#type),
             E::Register(reg)        => DisplayIndented::fmt(reg, indentation, f),
             E::Node{name, value}    => writeln!(f, "{}node {} = {}", indentation.lock(), name, value),
-            E::Memory(mem)          => DisplayIndented::fmt(mem, indentation, f),
+            E::Memory(mem)          => MemoryDecl(mem, Default::default()).fmt(indentation, f),
             E::Instance(inst)       => DisplayIndented::fmt(inst, indentation, f),
         }
     }
