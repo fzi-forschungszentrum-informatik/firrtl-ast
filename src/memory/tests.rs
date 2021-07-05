1bfe263bddf42e4cf3a849d33e9b78196d446d36
Julian Ganz <ganz@fzi.de>
2021-07-05 09:55:06 +0200

    memory: use new MemoryDecl for formatting in parse test
    The `DisplayIndented` impl for `Memory` will be removed in the future.

--- src/memory/tests.rs
+++ src/memory/tests.rs
@@ -6,13 +6,13 @@ use nom::combinator::all_consuming;
 use crate::indentation::{DisplayIndented, Indentation};
 use crate::tests::Equivalence;
 
-use super::{Memory, parsers};
+use super::{Memory, display::MemoryDecl, parsers};
 
 
 #[quickcheck]
 fn parse_memory(mut base: Indentation, original: Memory) -> Result<Equivalence<Memory>, String> {
     let mut s: String = Default::default();
-    original.fmt(&mut base, &mut s).map_err(|e| e.to_string())?;
+    MemoryDecl(&original, Default::default()).fmt(&mut base, &mut s).map_err(|e| e.to_string())?;
 
     let res = all_consuming(|i| parsers::memory(i, &mut base))(&s)
         .finish()
