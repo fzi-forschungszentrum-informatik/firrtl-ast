72a4234357de282a7e315f64cd9b5fc4cedeebea
Julian Ganz <ganz@fzi.de>
2021-07-06 17:11:32 +0200

    circuit: reintegrate circuit head parser into consumer factory
    We originally moved the circuit head parsing into its own function
    intending to make it part of the public API. However, we decided to
    introduce a fnction returning a `ModuleConsumer` directly. Hence, the
    existing function lost its justification given its size and only having
    a single caller.

--- src/circuit/parsers.rs
+++ src/circuit/parsers.rs
@@ -6,7 +6,7 @@ use nom::sequence::tuple;
 use crate::error::{ParseError, convert_error};
 use crate::info::parse as parse_info;
 use crate::module::parsers::Modules;
-use crate::parsers::{IResult, identifier, kw, le, op, spaced};
+use crate::parsers::{identifier, kw, le, op, spaced};
 
 
 /// Parse a Circuit
@@ -21,22 +21,11 @@ pub fn circuit(input: &str) -> Result<super::Circuit, ParseError> {
 /// will return a `ModuleConsumer` which will construct a `Circuit` from that
 /// input.
 pub fn consumer(input: &str) -> Result<super::ModuleConsumer<Modules, ParseError>, ParseError> {
-    let (input, (top_name, info)) = head(input).map_err(|e| convert_error(input, e))?;
-
-    Ok(super::ModuleConsumer::new(top_name, info, Modules::new(input)))
-}
-
-
-/// Parse a circuit's "head"
-///
-/// This parser parses the first line of a circuit's definition, which contains
-/// the top module's name and an optional info attribute. On success, the parser
-/// will yield a tuple with the top module's name as its first element and the
-/// info as its second.
-pub fn head<'i>(input: &'i str) -> IResult<'i, (&'i str, Option<String>)> {
-    map(
+    let (input, (top_name, info)) = map(
         tuple((kw("circuit"), spaced(identifier), spaced(op(":")), parse_info, le)),
         |(_, n, _, i, ..)| (n, i)
-    )(input)
+    )(input).map_err(|e| convert_error(input, e))?;
+
+    Ok(super::ModuleConsumer::new(top_name, info, Modules::new(input)))
 }
 
