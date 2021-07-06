bc6cdaef2a2068a4b251026511ef08c4e8e52f0b
Julian Ganz <ganz@fzi.de>
2021-07-06 12:05:47 +0200

    circuit: add dedicated parser for a circuit's head

--- src/circuit/parsers.rs
+++ src/circuit/parsers.rs
@@ -13,10 +13,7 @@ use crate::parsers::{Error, IResult, identifier, kw, le, op, spaced};
 pub fn circuit(input: &str) -> IResult<super::Circuit> {
     use nom::error::{ErrorKind as EK, ParseError};
 
-    let (input, (top_name, info)) = map(
-        tuple((kw("circuit"), spaced(identifier), spaced(op(":")), parse_info, le)),
-        |(_, n, _, i, ..)| (n, i)
-    )(input)?;
+    let (input, (top_name, info)) = head(input)?;
 
     let mut mod_parser = Modules::default();
     let mut indent = Indentation::root().sub();
@@ -26,3 +23,18 @@ pub fn circuit(input: &str) -> IResult<super::Circuit> {
         .ok_or_else(|| nom::Err::Error(Error::from_error_kind(input, EK::MapOpt)))?;
     modules.finish().map(|(i, _)| (i, res))
 }
+
+
+/// Parse a circuit's "head"
+///
+/// This parser parses the first line of a circuit's definition, which contains
+/// the top module's name and an optional info attribute. On success, the parser
+/// will yield a tuple with the top module's name as its first element and the
+/// info as its second.
+pub fn head<'i>(input: &'i str) -> IResult<'i, (&'i str, Option<String>)> {
+    map(
+        tuple((kw("circuit"), spaced(identifier), spaced(op(":")), parse_info, le)),
+        |(_, n, _, i, ..)| (n, i)
+    )(input)
+}
+
