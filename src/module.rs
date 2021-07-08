f9750bf1e1eae506b4d2ed338acd2f43660e2d52
Julian Ganz <ganz@fzi.de>
2021-07-08 11:36:34 +0200

    module: make Arbitrary impl for Module rely on impl for Kind
    In the past, `Kind` was used only for denoting a `Module` as either
    regular or external. Recently however, we let it carry data associated
    to a `Module` `Kind` and started using it for data storage inside
    `Module`. In short, we transfered some responsibility from `Module` to
    `Kind`.
    
    However, up until now `Module` would still generate and shrink data
    stored in a `Kind` on its own. However, thanks to recent changes, we can
    not rely on `Kind` to generate meaningful data and shrink it, too. As a
    result, the way we generate and shrink `Port`s changes, and we don't
    enforce a maximum number of ports for regular modules any more but rely
    on the statement list, which is used as a source of ports, being
    bounded.

--- src/module.rs
+++ src/module.rs
@@ -100,54 +100,52 @@ impl DisplayIndented for Module {
 #[cfg(test)]
 impl Arbitrary for Module {
     fn arbitrary(g: &mut Gen) -> Self {
+        use crate::stmt::{self, tests::stmt_exprs};
         use crate::tests::Identifier;
 
-        let max_ports = (usize::arbitrary(g) % 16) + 1;
         let name = Identifier::arbitrary(g).into();
-        match Kind::arbitrary(g) {
-            Kind::Regular{..} => tests::module_with_stmts(
-                name,
-                std::iter::from_fn(|| Some(Arbitrary::arbitrary(g))),
-                max_ports,
-            ),
-            Kind::External => {
-                let mut sub = Gen::new(g.size() / max_ports);
-                let ports = (0..max_ports).map(|_| Arbitrary::arbitrary(&mut sub));
-                Module::new(name, ports, Kind::External)
-            },
-        }
+        let kind: Kind = Arbitrary::arbitrary(g);
+        let ports: Vec<_> = if kind.statements().is_empty() {
+            let n = (usize::arbitrary(g) % 16) + 1;
+            let mut g = Gen::new(g.size() / n);
+            std::iter::from_fn(|| Some(Arbitrary::arbitrary(&mut g))).take(n).collect()
+        } else {
+            let ports: std::collections::HashMap<_, _> = kind
+                .statements()
+                .iter()
+                .flat_map(stmt_exprs)
+                .flat_map(expr::Expression::references)
+                .filter_map(|e| if let stmt::Entity::Port(p) = e.as_ref() { Some(p) } else { None })
+                .map(|p| (p.name(), p.clone()))
+                .collect();
+            ports.into_iter().map(|(_, v)| v).collect()
+        };
+
+        Module::new(name, ports, kind)
     }
 
     fn shrink(&self) -> Box<dyn Iterator<Item = Self>> {
-        let p = self.ports.clone();
-        let k = self.kind().clone();
         let res = crate::tests::Identifier::from(self.name())
             .shrink()
-            .map(move |n| Self::new(n.into(), p.clone(), k.clone()));
+            .map({
+                let p = self.ports.clone();
+                let k = self.kind().clone();
+                move |n| Self::new(n.into(), p.clone(), k.clone())
+            })
+            .chain(self.kind().shrink().map({
+                let n = self.name.clone();
+                let p = self.ports.clone();
+                move |k| Self::new(n.clone(), p.clone(), k)
+            }));
 
-        let n = self.name.clone();
-        match self.kind() {
-            Kind::Regular{..} => {
-                // For regular modules, we must maintain that all ports used in
-                // statements are defined for the module. Hence, we shrink the
-                // statement list and derive the ports from that list.
-                let max_stmts = self.statements().len();
-                let res = res.chain(self.statements()
-                    .to_vec()
-                    .shrink()
-                    .map(move |s| tests::module_with_stmts(n.clone(), s, usize::MAX))
-                    .filter(move |m| m.statements().len() < max_stmts));
-                Box::new(res)
-            },
-            Kind::External => {
-                // For external modules, we can just shrink the port list.
-                let res = res.chain(self
-                    .ports
-                    .shrink()
-                    .map(move |p| Self::new(n.clone(), p, Kind::External))
-                );
-                Box::new(res)
-            },
+        // We can shrink ports only if we don't have to accomoidate for any
+        // statements which could potentially reference them.
+        if self.statements().is_empty() {
+            let n = self.name.clone();
+            let k = self.kind().clone();
+            Box::new(res.chain(self.ports.shrink().map(move |p| Self::new(n.clone(), p, k.clone()))))
+        } else {
+            Box::new(res)
         }
     }
 }
