74daa8ad58642c52e424b38ad6c426efd936daeb
Julian Ganz <ganz@fzi.de>
2021-07-09 17:32:52 +0200

    memory: add simple memory
    FIRRTL apparently knows not only `mem`, but also `cmem` and `smem`,
    which appear to be simpler types of memory. As they are quite similar,
    we decided to throw both together in a single type.

--- src/memory/simple.rs
+++ src/memory/simple.rs
@@ -1,6 +1,60 @@
 //! "Simple" memories
 
+use std::fmt;
+use std::sync::Arc;
+
+use crate::types;
+
 use super::common::ReadUnderWrite;
+
+
+/// A "simple" FIRRTL memory
+///
+/// Instances of this type represent either a `cmem` or `smem`.
+#[derive(Clone, Debug, PartialEq)]
+pub struct Memory {
+    name: Arc<str>,
+    data_type: types::Type,
+    kind: Kind,
+}
+
+impl Memory {
+    /// Create a new simple memory
+    pub fn new(name: impl Into<Arc<str>>, data_type: impl Into<types::Type>, kind: Kind) -> Self {
+        Self {name: name.into(), data_type: data_type.into(), kind}
+    }
+
+    /// Retrieve the memory's name
+    pub fn name(&self) -> &Arc<str> {
+        &self.name
+    }
+
+    /// Retrieve the kind of simple memory
+    pub fn kind(&self) -> Kind {
+        self.kind
+    }
+}
+
+impl types::Typed for Memory {
+    type Err = Self;
+
+    type Type = types::Type;
+
+    fn r#type(&self) -> Result<Self::Type, Self::Err> {
+        Ok(self.data_type.clone())
+    }
+}
+
+impl fmt::Display for Memory {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let kind = self.kind();
+        write!(f, "{} {}: {}", kind.keyword(), self.name(), self.data_type)?;
+        if let Kind::Sequential(Some(ruw)) = kind {
+            write!(f, " {}", ruw)?;
+        }
+        Ok(())
+    }
+}
 
 
 /// Kind of simple memory
