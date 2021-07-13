dcc3b594245cf5b50e4bb21c912af7946af3e2f0
Julian Ganz <ganz@fzi.de>
2021-07-13 14:27:00 +0200

    memory: add type representing a simple memory port

--- src/memory/simple.rs
+++ src/memory/simple.rs
@@ -3,9 +3,10 @@
 use std::fmt;
 use std::sync::Arc;
 
+use crate::expr;
 use crate::types;
 
-use super::common::ReadUnderWrite;
+use super::common::{PortDir, ReadUnderWrite};
 
 
 /// A "simple" FIRRTL memory
@@ -73,6 +74,77 @@ impl Kind {
             Self::Combinatory   => "cmem",
             Self::Sequential(_) => "smem",
         }
+    }
+}
+
+
+/// A port for a simple memory
+#[derive(Clone, Debug, PartialEq)]
+pub struct Port<R: expr::Reference> {
+    name: Arc<str>,
+    mem: Arc<Memory>,
+    dir: Option<PortDir>,
+    addr: expr::Expression<R>,
+    clock: expr::Expression<R>,
+}
+
+impl<R: expr::Reference> Port<R> {
+    /// Create a new memory port
+    pub fn new(
+        name: impl Into<Arc<str>>,
+        mem: Arc<Memory>,
+        dir: Option<PortDir>,
+        addr: expr::Expression<R>,
+        clock: expr::Expression<R>,
+    ) -> Self {
+        Self {name: name.into(), mem, dir, addr, clock}
+    }
+
+    /// Retrieve the memory associated with this port
+    pub fn memory(&self) -> &Arc<Memory> {
+        &self.mem
+    }
+
+    /// Retrieve the direction of this port
+    pub fn direction(&self) -> Option<PortDir> {
+        self.dir
+    }
+
+    /// Retrieve the address
+    pub fn address(&self) -> &expr::Expression<R> {
+        &self.addr
+    }
+
+    /// Retrieve the clock driving this port
+    pub fn clock(&self) -> &expr::Expression<R> {
+        &self.clock
+    }
+}
+
+impl<R: expr::Reference> types::Typed for Port<R> {
+    type Err = Arc<Memory>;
+
+    type Type = types::Type;
+
+    fn r#type(&self) -> Result<Self::Type, Self::Err> {
+        self.memory()
+            .r#type()
+            .map_err(Arc::new)
+            .and_then(|t| t.vector_base().map(|t| t.as_ref().clone()).ok_or(self.mem.clone()))
+    }
+}
+
+impl<R: expr::Reference> expr::Reference for Port<R> {
+    fn name(&self) -> &str {
+        self.name.as_ref()
+    }
+
+    fn flow(&self) -> Option<expr::Flow> {
+        self.dir.map(|d| match d {
+            PortDir::Read       => expr::Flow::Source,
+            PortDir::Write      => expr::Flow::Sink,
+            PortDir::ReadWrite  => expr::Flow::Duplex,
+        })
     }
 }
 
